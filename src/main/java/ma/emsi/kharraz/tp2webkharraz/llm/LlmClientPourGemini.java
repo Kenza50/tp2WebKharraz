d7cbdb2819556ed2c208b45515a3fc28ac23d206
Kenza Kharraz <Kenza.kharraz5@gmail.com>
2025-11-02 23:11:14 +0100

    ajout de l’interface Assistant et configuration du service IA

--- src/main/java/ma/emsi/kharraz/tp2webkharraz/llm/LlmClientPourGemini.java
+++ src/main/java/ma/emsi/kharraz/tp2webkharraz/llm/LlmClientPourGemini.java
@@ -1,5 +1,6 @@
 package ma.emsi.kharraz.tp2webkharraz.llm;
 
+import dev.langchain4j.service.AiServices;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.google.ai.GoogleAiChatModel;
 import jakarta.enterprise.context.Dependent;
@@ -7,17 +8,18 @@ import jakarta.enterprise.context.Dependent;
 @Dependent
 public class LlmClientPourGemini {
 
-    private final ChatLanguageModel model;
+    private final Assistant assistant;
 
     public LlmClientPourGemini() {
         String geminiKey = System.getenv("GEMINI_KEY");
-        this.model = GoogleAiChatModel.builder()
+        ChatLanguageModel model = GoogleAiChatModel.builder()
                 .apiKey(geminiKey)
                 .modelName("gemini-1.5-flash")
                 .build();
+        this.assistant = AiServices.create(Assistant.class, model);
     }
 
     public String generate(String userMessage) {
-        return model.generate(userMessage);
+        return assistant.chat(userMessage);
     }
 }
