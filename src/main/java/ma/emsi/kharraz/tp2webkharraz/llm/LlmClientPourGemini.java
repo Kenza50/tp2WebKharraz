e3d941a2d9b3d4d88c3905ee296f64f23dd4f477
Kenza Kharraz <Kenza.kharraz5@gmail.com>
2025-11-02 23:14:53 +0100

    implémentation du constructeur LlmClient

--- src/main/java/ma/emsi/kharraz/tp2webkharraz/llm/LlmClientPourGemini.java
+++ src/main/java/ma/emsi/kharraz/tp2webkharraz/llm/LlmClientPourGemini.java
@@ -1,14 +1,18 @@
 package ma.emsi.kharraz.tp2webkharraz.llm;
 
-import dev.langchain4j.service.AiServices;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.google.ai.GoogleAiChatModel;
+import dev.langchain4j.service.AiServices;
 import jakarta.enterprise.context.Dependent;
 
 @Dependent
 public class LlmClientPourGemini {
 
+    public String systemRole;
     private final Assistant assistant;
+    private final ChatMemory chatMemory;
 
     public LlmClientPourGemini() {
         String geminiKey = System.getenv("GEMINI_KEY");
@@ -16,7 +20,11 @@ public class LlmClientPourGemini {
                 .apiKey(geminiKey)
                 .modelName("gemini-1.5-flash")
                 .build();
-        this.assistant = AiServices.create(Assistant.class, model);
+        this.chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        this.assistant = AiServices.builder(Assistant.class)
+                .chatLanguageModel(model)
+                .chatMemory(chatMemory)
+                .build();
     }
 
     public String generate(String userMessage) {
